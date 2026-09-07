3eb20ac1a1243122fca21277ff362001566e5463
Jonathan Ho <92621215+jxndela@users.noreply.github.com>
2026-09-07 09:11:29 +0800

    Update Ex2.vue

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -2,13 +2,16 @@
     import { ref } from 'vue'
 
     // Add code here
+    // static import of images - meaning the images are imported at build time and bundled with the application
     import wad from '/public/assets/wad.png'
+    // reactive import of images - meaning the images are imported at runtime and can be changed dynamically
     const me = ref('/public/assets/me.png')
 </script>
 
 <template>
 
     <!-- Add code here -->
-    <img :src="wad" alt="wad" width="300" height="200">
-    <img :src="me" alt="me" width="200" height="200">
+    <!-- v-bind used to bind the src attribute of the img tag to a reactive variable -->
+    <img v-bind:src="wad" alt="wad" width="200" height="200">
+    <img v-bind:src="me" alt="me" width="200" height="200">
 </template>
