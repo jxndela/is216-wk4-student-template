ae0440ebdc73c8cdb7cdab33b415813887be0994
Jonathan Ho <92621215+jxndela@users.noreply.github.com>
2026-09-07 08:50:12 +0800

    ex2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -2,11 +2,13 @@
     import { ref } from 'vue'
 
     // Add code here
-
+    import wad from '/public/assets/wad.png'
+    const me = ref('/public/assets/me.png')
 </script>
 
 <template>
 
     <!-- Add code here -->
-   
+    <img :src="wad" alt="wad" width="200" height="200">
+    <img :src="me" alt="me" width="200" height="200">
 </template>
