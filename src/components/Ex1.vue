df3b17585d0a296a32c123669c91e1eb2861678f
Jonathan Ho <92621215+jxndela@users.noreply.github.com>
2026-09-07 08:45:00 +0800

    complete

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -3,15 +3,17 @@
 
     // Add code here
     // challenge: use array  
-    
+    // ref with array of fruits and use v-for to display them in the template
+    const fruits = ref(['apple', 'banana', 'orange'])
+    const message = ref('I completed Ex1')
 </script>
 
 <template>
     {{ message }} 
-   
-    <label for=""> <input type="radio" name="fruit"> {{ fruit1 }} </label> 
-    <label for=""> <input type="radio" name="fruit"> {{ fruit2 }} </label> 
-    <label for=""><input type="radio" name="fruit"> {{ fruit3 }} </label> 
+
+    <label for=""> <input type="radio" name="fruit"> {{ fruits[0] }} </label> 
+    <label for=""> <input type="radio" name="fruit"> {{ fruits[1] }} </label> 
+    <label for=""><input type="radio" name="fruit"> {{ fruits[2] }} </label> 
     
 </template>
 
